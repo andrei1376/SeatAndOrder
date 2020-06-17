7697a9b66a09686201c84cebf270c252b00feb0a
Emiliano Baublys <39276721+andrei1376@users.noreply.github.com>
2020-06-17 05:14:43 +0200

    firstCommit

---
+++ SeatAndOrder/SeatAndOrder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeatAndOrder
+//
+//  Created by Emiliano Baublys on 17/06/2020.
+//  Copyright © 2020 Emiliano Baublys. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
