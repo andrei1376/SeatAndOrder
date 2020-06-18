0679991c23c30b2fcf417ee006c3b1b2f6915052
Emiliano Baublys <39276721+andrei1376@users.noreply.github.com>
2020-06-18 04:30:20 +0200

    Update SeatAndOrderTests.swift

--- SeatAndOrder/SeatAndOrderTests/SeatAndOrderTests.swift
+++ SeatAndOrder/SeatAndOrderTests/SeatAndOrderTests.swift
@@ -11,9 +11,7 @@ import XCTest
 
 class SeatAndOrderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
